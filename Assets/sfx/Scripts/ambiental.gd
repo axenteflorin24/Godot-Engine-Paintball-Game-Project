7bc375b98d4651b2d76329d0834e8a90eef3e117
Axente Florin <148908673+axenteflorin24@users.noreply.github.com>
2026-05-07 02:18:38 -0700

    Add files via upload

---
+++ Assets/sfx/Scripts/ambiental.gd
@@ -0,0 +1,26 @@
+extends AudioStreamPlayer
+
+var track = "A"
+
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var _delta = delta
+
+	if $ambientalLoop.is_stopped():
+		
+		if track == "A":
+			track = "B"
+			$"$ambiental_loop".set("volume_db", "-8.0")
+			$"$ambiental_loop".play()
+			$".".set("volume_db", "-80.0")
+			$".".stop()
+		if track == "B":
+			track = "A"
+			$".".set("volume_db", "-8.0")
+			$".".play()
+			$"$ambiental_loop".set("volume_db", "-80.0")
+			$"$ambiental_loop".stop()
+		$ambientalLoop.start()
